b953473f663fa5f8582ba725513cf85971787230
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2022-10-10 11:41:04 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
@@ -18,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+reportWebVitals();
